e25d163a32263c088f57e90410c0b31aff313b0c
Francelin <67784025+Francel1n@users.noreply.github.com>
2020-11-16 15:59:45 +0100

    Update Recettes.rb

--- Recettes.rb
+++ Recettes.rb
@@ -1,3 +1,15 @@
-puts "hello\n\n\nworld"
+puts "Comment tu t'appelles ?"
 inp = gets
-puts "Bonjour" inp "Comment vas-tu ?"
+puts "Et comment vas-tu"
+puts inp
+puts "?"
+
+require 'rubygems'
+require 'json'
+
+s = '{"gold":1271,"silver":1284, "platinum": 1270}'
+obj = JSON.parse(s)
+
+puts obj["gold"]
+puts obj["silver"]
+puts obj["platinum"]
