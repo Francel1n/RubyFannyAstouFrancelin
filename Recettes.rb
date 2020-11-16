a527f514425b363e5fc5e1c9e64efa45a84dfc26
Francelin <67784025+Francel1n@users.noreply.github.com>
2020-11-16 16:35:23 +0100

    Create Recettes.rb

---
+++ Recettes.rb
@@ -0,0 +1,24 @@
+puts "Comment tu t'appelles ?"
+inp = gets
+puts "Et comment vas-tu"
+puts inp
+puts "?"
+puts "Quelle recette souhaites-tu voir ?"
+
+inp = gets
+
+require 'rubygems'
+require 'json'
+
+s = '{"gold":1271,"silver":1284, "platinum": 1270}'
+obj = JSON.parse(s)
+
+if inp == "gold" then
+  puts obj["gold"]
+end
+if inp = "silver" then
+  puts obj["silver"]
+end
+if inp = "platinium" then
+  puts obj["platinum"]
+end
