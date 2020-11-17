3232f928aad702de0e23435ddd2d6417a868905b
Fanny Armand <fannyarmand2@gmail.com>
2020-11-17 17:25:41 +0100

    possibilité de recette proposée selon deux ingrédients

--- Recettes.rb
+++ Recettes.rb
@@ -16,6 +16,23 @@ recipesList = JSON.parse(response.body)
 #ingredient = "Sucre"
 #puts !recipesList[1]["ingredients"][ingredient].nil?
 
+#a partir des ingrédients proposer une recette
+puts "Quels ingédients avez-vous?"
+ingredients = gets.chomp.to_s
+ingredientList = ingredients.split(",")
+ingredientList.each do |item|
+  puts item
+end
+recipesList.each do |recipe|
+  if recipe["ingredients"][ingredientList[0]] && recipe["ingredients"][ingredientList[1]]
+    puts recipe["recipe_name"]
+    puts recipe["ingredients"]
+    puts recipe["steps"]
+  end
+end
+
+
+
 #afficher la recette demandée
 puts "Quelle est la recette que vous désirez réaliser?"
 recette = gets.chomp
@@ -27,8 +44,3 @@ recipesList.each do |item|
     puts item["steps"]
   end
 end
-
-#a partir des ingrédients proposer une recette
-
-puts "Quels ingédients avez-vous?"
-ingredients = gets.chomp
